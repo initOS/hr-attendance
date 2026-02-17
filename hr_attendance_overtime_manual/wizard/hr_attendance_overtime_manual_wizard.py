d264076073e8df84ee7ed5fac916dd617914b006
Mathias Francke <mathias.francke@ext.initos.com>
2026-02-17 11:53:24 +0100

    [MIG] hr_attendance_overtime_manual: Migration to 18.0
    [FIX] fixed invalid user
    
    [FIX] fixed overtime manual wizard
    
    [ADD] bring back button from old module
    
    removed TODO comment

--- hr_attendance_overtime_manual/wizard/hr_attendance_overtime_manual_wizard.py
+++ hr_attendance_overtime_manual/wizard/hr_attendance_overtime_manual_wizard.py
@@ -1,12 +1,12 @@
 # © 2023 initOS GmbH
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, fields, models
+from odoo import fields, models
 
 
 class TimesheetWizard(models.TransientModel):
     _name = "hr.attendance.overtime.wizard"
-    _description = _("Wizard to add manual overtime")
+    _description = "Wizard to add manual overtime"
 
     date = fields.Date(
         string="Day", required=True, default=lambda _: fields.Date.today()
@@ -17,7 +17,7 @@ class TimesheetWizard(models.TransientModel):
     def action_create(self):
         self.env["hr.attendance.overtime"].create(
             {
-                "employee_id": self.env.context.get("id"),  # TODO: Is this correct?
+                "employee_id": self.env.context.get("active_id"),
                 "date": self.date,
                 "duration": self.duration,
                 "note": self.note,
